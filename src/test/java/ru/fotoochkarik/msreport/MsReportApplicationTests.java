9bf2ff40fa1119d178db14097d21eabc57b0167b
Vitaliy <v.shelkunov@premiumitsolution.com>
2023-03-11 12:50:13 +0300

    Initial commit

---
+++ src/test/java/ru/fotoochkarik/msreport/MsReportApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fotoochkarik.msreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReportApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
